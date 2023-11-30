a0cc9701783e4d3454d9a6d1a19ce61ed0355cf1
dwantscher_ext <dominique.wantscher@rosenberger.com>
2023-11-30 13:28:46 +0100

    changed read file to system.io.file

--- Update-FolderStructure.ps1
+++ Update-FolderStructure.ps1
@@ -13,16 +13,16 @@ foreach ($year in @(2015..$date.Year))
         {
             $null = New-Item -ItemType Directory -Path "$year\$day"
         }
-        if (!(Test-Path "$year\$day\input.txt"))
+        if (!(Test-Path "$year\$day\aoc_input.txt"))
         {
             if ($year -lt $date.Year -or ($date.Month -eq 12 -and $day -le $date.Day))
             {
-                [System.IO.File]::AppendAllText("$year\$day\input.txt", ((Invoke-WebRequest -Uri "https://adventofcode.com/$year/day/$day/input" -WebSession $session).Content))
+                [System.IO.File]::AppendAllText("$year\$day\aoc_input.txt", ((Invoke-WebRequest -Uri "https://adventofcode.com/$year/day/$day/input" -WebSession $session).Content))
             }
         }
-        if (!(Test-Path "$year\$day\sample.txt"))
+        if (!(Test-Path "$year\$day\aoc_sample.txt"))
         {
-            [System.IO.File]::AppendAllText("$year\$day\sample.txt",'')
+            [System.IO.File]::AppendAllText("$year\$day\aoc_sample.txt",'')
         }
         $parts = "part1", "part2"
         foreach ($part in $parts)
@@ -31,8 +31,8 @@ foreach ($year in @(2015..$date.Year))
             {
                 [System.IO.File]::AppendAllText("$year\$day\$part.ps1","# https://adventofcode.com/$year/day/$day" + [Environment]::NewLine)
                 [System.IO.File]::AppendAllText("$year\$day\$part.ps1",'' + [Environment]::NewLine)
-                [System.IO.File]::AppendAllText("$year\$day\$part.ps1",'$sample = [System.IO.File]::ReadAllLines("sample.txt")' + [Environment]::NewLine)
-                [System.IO.File]::AppendAllText("$year\$day\$part.ps1",'$input = [System.IO.File]::ReadAllLines("input.txt")' + [Environment]::NewLine)
+                [System.IO.File]::AppendAllText("$year\$day\$part.ps1",'$sample = [System.IO.File]::ReadAllLines("aoc_sample.txt")' + [Environment]::NewLine)
+                [System.IO.File]::AppendAllText("$year\$day\$part.ps1",'$input = [System.IO.File]::ReadAllLines("aoc_input.txt")' + [Environment]::NewLine)
                 [System.IO.File]::AppendAllText("$year\$day\$part.ps1",'' + [Environment]::NewLine)
             }
         }
