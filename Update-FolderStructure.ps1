9a6aea25ab6277136ec227cceb69531905b3ad21
dwantscher_ext <dominique.wantscher@rosenberger.com>
2023-11-30 12:48:08 +0100

    changed out-file to system.io.file

--- Update-FolderStructure.ps1
+++ Update-FolderStructure.ps1
@@ -17,22 +17,22 @@ foreach ($year in @(2015..$date.Year))
         {
             if ($year -lt $date.Year -or ($date.Month -eq 12 -and $day -le $date.Day))
             {
-                (Invoke-WebRequest -Uri "https://adventofcode.com/$year/day/$day/input" -WebSession $session).Content | Out-File -FilePath "$year\$day\input.txt"
+                [System.IO.File]::WriteAllText("$year\$day\input.txt", ((Invoke-WebRequest -Uri "https://adventofcode.com/$year/day/$day/input" -WebSession $session).Content | Out-File -FilePath))
             }
         }
         if (!(Test-Path "$year\$day\sample.txt"))
         {
-            New-Item -ItemType File -Path "$year\$day\sample.txt"
+            [System.IO.File]::WriteAllText("$year\$day\sample.txt",'')
         }
         $parts = "part1", "part2"
         foreach ($part in $parts)
         {
             if (!(Test-Path "$year\$day\$part"))
             {
-                "# https://adventofcode.com/$year/day/$day" | Out-File -FilePath "$year\$day\$part.ps1"
-                '' | Out-File -FilePath "$year\$day\$part.ps1" -Append
-                '$input = Get-Content -Path "sample.txt"' | Out-File -FilePath "$year\$day\$part.ps1" -Append
-                '#$input = Get-Content -Path "input.txt"' | Out-File -FilePath "$year\$day\$part.ps1" -Append
+                [System.IO.File]::WriteAllText("$year\$day\$part.ps1","# https://adventofcode.com/$year/day/$day")
+                [System.IO.File]::AppendAllText("$year\$day\$part.ps1",'')
+                [System.IO.File]::AppendAllText("$year\$day\$part.ps1",'$input = Get-Content -Path "sample.txt"')
+                [System.IO.File]::AppendAllText("$year\$day\$part.ps1",'#$input = Get-Content -Path "input.txt"')
             }
         }
     }
