fde06335bb4e6c22442452c9ea46ee83b8787305
Domanaik <63446985+Domanaik@users.noreply.github.com>
2023-12-01 11:41:22 +0100

    solution day 1

--- 2023/1/part1.ps1
+++ 2023/1/part1.ps1
@@ -1,5 +1,14 @@
 # https://adventofcode.com/2023/day/1
 
-$aoc_sample = [System.IO.File]::ReadAllLines("sample.txt")
-$aoc_input = [System.IO.File]::ReadAllLines("input.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2023\1\sample1.txt")
+$aoc_input = [System.IO.File]::ReadAllLines("2023\1\input.txt")
+$calibration_value = 0
+$regex = '\d'
 
+foreach ($line in $aoc_input)
+{
+    $firstDigit = [Regex]::Match($line,$regex).Value
+    $secondDigit = [Regex]::Match($line,$regex,[System.Text.RegularExpressions.RegexOptions]::RightToLeft).Value
+    $calibration_value += ($firstDigit*10+$secondDigit)
+}
+$calibration_value
