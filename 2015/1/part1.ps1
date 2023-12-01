60120fa95af2bced21e85bbd4a0bea9214dd4af9
Domanaik <63446985+Domanaik@users.noreply.github.com>
2023-12-01 21:46:53 +0100

    solution day 1

--- 2015/1/part1.ps1
+++ 2015/1/part1.ps1
@@ -1,5 +1,15 @@
 # https://adventofcode.com/2015/day/1
 
-$aoc_sample = [System.IO.File]::ReadAllLines("sample.txt")
-$aoc_input = [System.IO.File]::ReadAllLines("input.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2015\1\sample.txt")
+$aoc_input = [System.IO.File]::ReadAllLines("2015\1\input.txt")
 
+$floor = 0
+foreach ($char in $($aoc_input).ToCharArray())
+{
+    switch ($char)
+    {
+        "(" { $floor++ }
+        ")" { $floor-- }
+    }
+}
+$floor
--- 2015/1/part2.ps1
+++ 2015/1/part2.ps1
@@ -1,5 +1,21 @@
 # https://adventofcode.com/2015/day/1
 
-$aoc_sample = [System.IO.File]::ReadAllLines("sample.txt")
-$aoc_input = [System.IO.File]::ReadAllLines("input.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2015\1\sample.txt")
+$aoc_input = [System.IO.File]::ReadAllLines("2015\1\input.txt")
 
+$floor = 0
+$position = 0
+foreach ($char in $($aoc_input).ToCharArray())
+{
+    $position++
+    switch ($char)
+    {
+        "(" { $floor++ }
+        ")" { $floor-- }
+    }
+    if ($floor -eq -1)
+    {
+        $position
+        break
+    }
+}
