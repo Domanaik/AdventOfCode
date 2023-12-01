9e1deccfa1b130b8771222de9a4220c6012e6c40
Domanaik <63446985+Domanaik@users.noreply.github.com>
2023-12-01 22:57:12 +0100

    solution day 2

--- 2015/2/part1.ps1
+++ 2015/2/part1.ps1
@@ -1,5 +1,20 @@
 # https://adventofcode.com/2015/day/2
 
-$aoc_sample = [System.IO.File]::ReadAllLines("sample.txt")
-$aoc_input = [System.IO.File]::ReadAllLines("input.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2015\2\sample.txt")
+$aoc_input = [System.IO.File]::ReadAllLines("2015\2\input.txt")
 
+$squarefeet = 0
+
+foreach ($line in $aoc_input)
+{
+    $lwh = [Regex]::Matches($line,'\d+')
+    $l = [int]$lwh[0].Value
+    $w = [int]$lwh[1].Value
+    $h = [int]$lwh[2].Value
+    $lw = $l*$w
+    $wh = $w*$h
+    $hl = $h*$l
+    $minimum = ($lw,$wh,$hl | Measure-Object -Minimum).Minimum
+    $squarefeet += (2*$lw + 2*$wh + 2*$hl + $minimum)
+}
+$squarefeet
--- 2015/2/part2.ps1
+++ 2015/2/part2.ps1
@@ -1,5 +1,15 @@
 # https://adventofcode.com/2015/day/2
 
-$aoc_sample = [System.IO.File]::ReadAllLines("sample.txt")
-$aoc_input = [System.IO.File]::ReadAllLines("input.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2015\2\sample.txt")
+$aoc_input = [System.IO.File]::ReadAllLines("2015\2\input.txt")
 
+$totalribbon  = 0
+
+foreach ($line in $aoc_input)
+{
+    $lwh = [Regex]::Matches($line,'\d+')
+    $array = [int]$lwh[0].Value, [int]$lwh[1].Value, [int]$lwh[2].Value | Sort-Object
+    $ribbon = 2*$array[0] + 2*$array[1] + $array[0]*$array[1]*$array[2] 
+    $totalribbon += $ribbon
+}
+$totalribbon
