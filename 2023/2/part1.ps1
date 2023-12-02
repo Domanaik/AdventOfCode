b32eec0ba0a9fcca53cfdcbdc602dfc4b0c21264
Domanaik <63446985+Domanaik@users.noreply.github.com>
2023-12-02 11:10:56 +0100

    solution day 2

--- 2023/2/part1.ps1
+++ 2023/2/part1.ps1
@@ -1,5 +1,45 @@
 # https://adventofcode.com/2023/day/2
 
-$aoc_sample = [System.IO.File]::ReadAllLines("sample.txt")
-$aoc_input = [System.IO.File]::ReadAllLines("input.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2023\2\sample.txt")
+$aoc_input = [System.IO.File]::ReadAllLines("2023\2\input.txt")
 
+$sum_games = 0
+$red_cubes = 12
+$green_cubes = 13
+$blue_cubes = 14
+
+foreach ($line in $aoc_input)
+{
+    $parts = $line -split ":" -split ";"
+    $possible = $true
+    foreach ($reveal in ($parts | Select-Object -Skip 1))
+    {
+        foreach ($color in ($reveal -split ","))
+        {
+            $color = $color -split " "
+            switch ($color[2])
+            {
+                "red" { 
+                    if ([int]$color[1] -gt $red_cubes) {
+                        $possible = $false
+                    }
+                }
+                "green" { 
+                    if ([int]$color[1] -gt $green_cubes) {
+                        $possible = $false
+                    }
+                }
+                "blue" { 
+                    if ([int]$color[1] -gt $blue_cubes) {
+                        $possible = $false
+                    }
+                }
+            }
+        }
+    }
+    if ($possible)
+    {
+        $sum_games += ($parts[0] -split ' ','')[1]
+    }
+}
+$sum_games
--- 2023/2/part2.ps1
+++ 2023/2/part2.ps1
@@ -1,5 +1,41 @@
 # https://adventofcode.com/2023/day/2
 
-$aoc_sample = [System.IO.File]::ReadAllLines("sample.txt")
-$aoc_input = [System.IO.File]::ReadAllLines("input.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2023\2\sample.txt")
+$aoc_input = [System.IO.File]::ReadAllLines("2023\2\input.txt")
 
+$sum_power = 0
+
+foreach ($line in $aoc_input)
+{
+    $red_cubes = 0
+    $green_cubes = 0
+    $blue_cubes = 0
+    $parts = $line -split ":" -split ";"
+    foreach ($reveal in ($parts | Select-Object -Skip 1))
+    {
+        foreach ($color in ($reveal -split ","))
+        {
+            $color = $color -split " "
+            switch ($color[2])
+            {
+                "red" { 
+                    if ($red_cubes -lt [int]$color[1]) {
+                        $red_cubes = [int]$color[1]
+                    }
+                }
+                "green" { 
+                    if ($green_cubes -lt [int]$color[1]) {
+                        $green_cubes = [int]$color[1]
+                    }
+                }
+                "blue" { 
+                    if ($blue_cubes -lt [int]$color[1]) {
+                        $blue_cubes = [int]$color[1]
+                    }
+                }
+            }
+        }
+    }
+    $sum_power += $red_cubes * $green_cubes * $blue_cubes
+}
+$sum_power
