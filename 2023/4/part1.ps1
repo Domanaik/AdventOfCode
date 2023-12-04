5fbb4afdaca3327174e9cbe62d01f7e0b1443ce9
Domanaik <63446985+Domanaik@users.noreply.github.com>
2023-12-04 11:08:02 +0100

    solution day 4

--- 2023/4/part1.ps1
+++ 2023/4/part1.ps1
@@ -1,5 +1,22 @@
 # https://adventofcode.com/2023/day/4
 
-$aoc_sample = [System.IO.File]::ReadAllLines("sample.txt")
-$aoc_input = [System.IO.File]::ReadAllLines("input.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2023\4\sample.txt")
+$aoc_sample = [System.IO.File]::ReadAllLines("2023\4\input.txt")
 
+$regex = '\d+'
+$totalpoints = 0
+foreach ($line in $aoc_sample) {
+    $cards = $line -split ":" -split "\|"
+    $numbersyouhave = [Regex]::Matches($cards[1], $regex).Value
+    $winningnumbers = [Regex]::Matches($cards[2], $regex).Value
+    $points = 0
+    foreach ($numberyouhave in $numbersyouhave) {
+        if ($winningnumbers -contains $numberyouhave) {
+            $points++
+        }
+    }
+    if ($points -ge 1) {
+        $totalpoints += [System.Math]::Pow(2, $points - 1)
+    }
+}
+$totalpoints
--- 2023/4/part2.ps1
+++ 2023/4/part2.ps1
@@ -1,5 +1,24 @@
 # https://adventofcode.com/2023/day/4
 
-$aoc_sample = [System.IO.File]::ReadAllLines("sample.txt")
-$aoc_input = [System.IO.File]::ReadAllLines("input.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2023\4\sample.txt")
+$aoc_sample = [System.IO.File]::ReadAllLines("2023\4\input.txt")
 
+$regex = '\d+'
+$copies = New-Object int[] ($aoc_sample.Length + 1)
+foreach ($line in $aoc_sample) {
+    $cards = $line -split ":" -split "\|"
+    $card = [int][Regex]::Matches($cards[0], $regex).Value
+    $copies[$card]++
+    $numbersyouhave = [Regex]::Matches($cards[1], $regex).Value
+    $winningnumbers = [Regex]::Matches($cards[2], $regex).Value
+    $matchingnumbers = 0
+    foreach ($numberyouhave in $numbersyouhave) {
+        if ($winningnumbers -contains $numberyouhave) {
+            $matchingnumbers++
+        }
+    }
+    for ($i = 1; $i -le $matchingnumbers; $i++) {
+        $copies[$card + $i] += $copies[$card]
+    }
+}
+($copies | Measure-Object -Sum).Sum
