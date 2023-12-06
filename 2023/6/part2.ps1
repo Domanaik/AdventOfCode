152138ac1f25837963d412728b3f37670fc14b27
Domanaik <63446985+Domanaik@users.noreply.github.com>
2023-12-06 11:17:19 +0100

    optimizied by precasting type and break

--- 2023/6/part2.ps1
+++ 2023/6/part2.ps1
@@ -1,17 +1,14 @@
-# https://adventofcode.com/2023/day/4
+# https://adventofcode.com/2023/day/6
 
 #$aoc_sample = [System.IO.File]::ReadAllLines("2023\6\sample.txt")
 $aoc_sample = [System.IO.File]::ReadAllLines("2023\6\input.txt")
 
-$time = ([Regex]::Matches($aoc_sample[0], '\d+').Value) -join ''
-$distance = ([Regex]::Matches($aoc_sample[1], '\d+').Value) -join ''
-
-$wins = 0
+[int64]$time = ([Regex]::Matches($aoc_sample[0], '\d+').Value) -join ''
+[int64]$distance = ([Regex]::Matches($aoc_sample[1], '\d+').Value) -join ''
 
 for ($i = 1; $i -lt $time; $i++) {
     if (($time - $i) * $i -gt $distance) {
-        $wins++
+        $time - 2 * $i + 1
+        break
     }
 }
-
-$wins
