e944af3f9fff4ed7681684163c3d518d1cf5fa34
Domanaik <63446985+Domanaik@users.noreply.github.com>
2023-12-06 09:39:39 +0100

    solution day 6

--- 2023/6/part1.ps1
+++ 2023/6/part1.ps1
@@ -1,5 +1,22 @@
-# https://adventofcode.com/2023/day/6
+# https://adventofcode.com/2023/day/4
 
-$aoc_sample = [System.IO.File]::ReadAllLines("sample.txt")
-$aoc_input = [System.IO.File]::ReadAllLines("input.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2023\6\sample.txt")
+$aoc_sample = [System.IO.File]::ReadAllLines("2023\6\input.txt")
 
+$times = ([Regex]::Matches($aoc_sample[0], '\d+').Value)
+$distances = ([Regex]::Matches($aoc_sample[1], '\d+').Value)
+
+$wins = New-Object int[] $times.Count
+
+for ($i = 0; $i -lt $times.Count; $i++) {
+    for ($j = 1; $j -lt $times[$i]; $j++) {
+        if (($times[$i] - $j) * $j -gt $distances[$i]) {
+            $wins[$i]++
+        }
+    }
+}
+$result = 1
+foreach ($win in $wins) {
+    $result *= $win
+}
+$result
--- 2023/6/part2.ps1
+++ 2023/6/part2.ps1
@@ -1,5 +1,17 @@
-# https://adventofcode.com/2023/day/6
+# https://adventofcode.com/2023/day/4
 
-$aoc_sample = [System.IO.File]::ReadAllLines("sample.txt")
-$aoc_input = [System.IO.File]::ReadAllLines("input.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2023\6\sample.txt")
+$aoc_sample = [System.IO.File]::ReadAllLines("2023\6\input.txt")
 
+$time = ([Regex]::Matches($aoc_sample[0], '\d+').Value) -join ''
+$distance = ([Regex]::Matches($aoc_sample[1], '\d+').Value) -join ''
+
+$wins = 0
+
+for ($i = 1; $i -lt $time; $i++) {
+    if (($time - $i) * $i -gt $distance) {
+        $wins++
+    }
+}
+
+$wins
