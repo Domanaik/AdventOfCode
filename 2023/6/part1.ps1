c3e10890e27ad4e8c99c31901a56c4c23ed4afc8
Domanaik <63446985+Domanaik@users.noreply.github.com>
2023-12-06 11:30:23 +0100

    optimizied by precasting type and break

--- 2023/6/part1.ps1
+++ 2023/6/part1.ps1
@@ -3,20 +3,16 @@
 #$aoc_sample = [System.IO.File]::ReadAllLines("2023\6\sample.txt")
 $aoc_sample = [System.IO.File]::ReadAllLines("2023\6\input.txt")
 
-$times = ([Regex]::Matches($aoc_sample[0], '\d+').Value)
-$distances = ([Regex]::Matches($aoc_sample[1], '\d+').Value)
+[int[]]$times = ([Regex]::Matches($aoc_sample[0], '\d+').Value)
+[int[]]$distances = ([Regex]::Matches($aoc_sample[1], '\d+').Value)
 
-$wins = New-Object int[] $times.Count
-
+$wins = 1
 for ($i = 0; $i -lt $times.Count; $i++) {
     for ($j = 1; $j -lt $times[$i]; $j++) {
         if (($times[$i] - $j) * $j -gt $distances[$i]) {
-            $wins[$i]++
+            $wins *= $times[$i] - 2 * $j + 1
+            break
         }
     }
 }
-$result = 1
-foreach ($win in $wins) {
-    $result *= $win
-}
-$result
+$wins
