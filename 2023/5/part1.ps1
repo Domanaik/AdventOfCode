09617151df5e4c0fe9193145b4f62dec5b53cee3
Domanaik <63446985+Domanaik@users.noreply.github.com>
2023-12-06 16:33:58 +0100

    fixed day in header

--- 2023/5/part1.ps1
+++ 2023/5/part1.ps1
@@ -1,4 +1,4 @@
-# https://adventofcode.com/2023/day/4
+# https://adventofcode.com/2023/day/5
 
 #$aoc_sample = [System.IO.File]::ReadAllLines("2023\5\sample.txt")
 $aoc_sample = [System.IO.File]::ReadAllLines("2023\5\input.txt")
--- 2023/6/part1.ps1
+++ 2023/6/part1.ps1
@@ -1,4 +1,4 @@
-# https://adventofcode.com/2023/day/4
+# https://adventofcode.com/2023/day/6
 
 #$aoc_sample = [System.IO.File]::ReadAllLines("2023\6\sample.txt")
 $aoc_sample = [System.IO.File]::ReadAllLines("2023\6\input.txt")
