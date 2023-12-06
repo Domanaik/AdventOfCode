125b8eb3821a7b5fc1e46955ad79bf64de9b4cac
Domanaik <63446985+Domanaik@users.noreply.github.com>
2023-12-06 13:30:04 +0100

    clean up

--- 2023/6/part2.ps1
+++ 2023/6/part2.ps1
@@ -3,12 +3,9 @@
 #$aoc_sample = [System.IO.File]::ReadAllLines("2023\6\sample.txt")
 $aoc_sample = [System.IO.File]::ReadAllLines("2023\6\input.txt")
 
-[int64]$time = ([Regex]::Matches($aoc_sample[0], '\d+').Value) -join ''
-[int64]$distance = ([Regex]::Matches($aoc_sample[1], '\d+').Value) -join ''
+[int64]$p = ([Regex]::Matches($aoc_sample[0], '\d+').Value) -join ''
+[int64]$q = ([Regex]::Matches($aoc_sample[1], '\d+').Value) -join ''
 
-$p = - $time
-$q = $distance
 $x1 = - ($p / 2) - [math]::sqrt([math]::pow($p / 2, 2) - $q)
 $x2 = - ($p / 2) + [math]::sqrt([math]::pow($p / 2, 2) - $q)
-
 [int][Math]::Ceiling($x2 - $x1)
