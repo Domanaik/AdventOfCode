cac715df5b77360a88524775027144139cd197b0
Domanaik <63446985+Domanaik@users.noreply.github.com>
2023-12-06 13:17:30 +0100

    changed to pq-formula

--- 2023/6/part2.ps1
+++ 2023/6/part2.ps1
@@ -6,9 +6,9 @@ $aoc_sample = [System.IO.File]::ReadAllLines("2023\6\input.txt")
 [int64]$time = ([Regex]::Matches($aoc_sample[0], '\d+').Value) -join ''
 [int64]$distance = ([Regex]::Matches($aoc_sample[1], '\d+').Value) -join ''
 
-for ($i = 1; $i -lt $time; $i++) {
-    if (($time - $i) * $i -gt $distance) {
-        $time - 2 * $i + 1
-        break
-    }
-}
+$p = - $time
+$q = $distance
+$x1 = - ($p / 2) - [math]::sqrt([math]::pow($p / 2, 2) - $q)
+$x2 = - ($p / 2) + [math]::sqrt([math]::pow($p / 2, 2) - $q)
+
+[int][Math]::Ceiling($x2 - $x1)
