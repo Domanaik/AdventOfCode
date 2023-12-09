0dd09ac222402807512d1b2756383e39d3375c01
Domanaik <63446985+Domanaik@users.noreply.github.com>
2023-12-09 16:23:54 +0100

    sample is working, but input is not :/

--- 2023/9/part1.ps1
+++ 2023/9/part1.ps1
@@ -3,12 +3,10 @@
 using namespace System.Collections.Generic
 
 $aoc_sample = [System.IO.File]::ReadAllLines("2023\9\sample.txt")
-$aoc_sample = [System.IO.File]::ReadAllLines("2023\9\input.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2023\9\input.txt")
 
 class Sequence {
     [List[int64]]$List
-    [Sequence]$NextSequence
-    [int64]$Sum
 
     [void]addList([List[int64]]$list) {
         $this.List = $list
@@ -24,7 +22,6 @@ class Sequence {
     }
 
     [void]getNextSequence() {
-        
         Write-Host $this.List
         if ($this.isAllZeroes()) {
             $this.List.Add(0)
@@ -32,44 +29,26 @@ class Sequence {
         }
         else {
             $sequence = [Sequence]::new()
-
             [List[int64]]$newList = @()
             for ($i = 0; $i -lt $this.List.Count - 1; $i++) {
                 $newList.Add(($this.List[$i + 1] - $this.List[$i]))
             }
             $sequence.addList($newList)
             $sequence.getNextSequence()
-            $this.NextSequence = $sequence
-            $this.Sum = $this.List[-1] + $this.NextSequence.List[-1]
-            $this.List.Add($this.Sum)
+            $this.List.Add($this.List[-1] + $sequence.List[-1])
             Write-Host $this.List
         }
     }
 }
 
-class SequenceList {
-    [List[Sequence]]$List
-    [int64]$Sum
-
-    [void]addList([Sequence]$list) {
-        $this.List += $list
-    }
-
-    [void]addSum([int64]$sum) {
-        $this.Sum += $sum
-    }
-}
-
-$sequenceList = [SequenceList]::new()
+$sum = 0
 foreach ($line in $aoc_sample) {
     $sequence = [Sequence]::new()
     $sequence.addList([Regex]::Matches($line, '\d+').Value)
     $sequence.getNextSequence()
-    $sequenceList.addList($sequence)
-    $sequenceList.Sum += $sequence.Sum
+    $sum += $sequence.List[-1]
     ""
-    pause
+    #pause
     ""
 }
-
-$sequenceList.Sum
+$sum
