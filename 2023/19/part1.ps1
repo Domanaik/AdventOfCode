dd8fd1ca7b007a2a1a3308b96d6c4655faa73e86
Domanaik <63446985+Domanaik@users.noreply.github.com>
2023-12-20 09:34:00 +0100

    Initial commit

--- 2023/19/part1.ps1
+++ 2023/19/part1.ps1
@@ -3,5 +3,3 @@
 $aoc_sample = [System.IO.File]::ReadAllLines("2023\19\sample.txt")
 #$aoc_sample = [System.IO.File]::ReadAllLines("2023\19\input.txt")
 
-# https://de.wikipedia.org/wiki/Gau%C3%9Fsche_Trapezformel
-# https://de.wikipedia.org/wiki/Satz_von_Pick
---
+++ 2023/20/part2.ps1
@@ -0,0 +1,6 @@
+# https://adventofcode.com/2023/day/20#part2
+
+$aoc_sample = [System.IO.File]::ReadAllLines("2023\20\sample1.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2023\20\sample2.txt")
+#$aoc_sample = [System.IO.File]::ReadAllLines("2023\20\input.txt")
+
