8efec76e32ca815d475efdf3e3deb695f53c9ff2
Domanaik <63446985+Domanaik@users.noreply.github.com>
2024-12-01 12:55:25 +0100

    Solution day 1

---
+++ 2024/1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            int[] left = lines.Select(line => int.Parse(line.Split([' ', '\t'], StringSplitOptions.RemoveEmptyEntries)[0])).ToArray();
+            int[] right = lines.Select(line => int.Parse(line.Split([' ', '\t'], StringSplitOptions.RemoveEmptyEntries)[1])).ToArray();
+            Array.Sort(left);
+            Array.Sort(right);
+
+            int totalDistance = left.Zip(right, (l, r) => Math.Abs(l - r)).Sum();
+            Console.WriteLine(totalDistance);
+
+            int similarityScore = left.Select(value => value * right.Count(r => r == value)).Sum();
+            Console.WriteLine(similarityScore);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+    }
+}
