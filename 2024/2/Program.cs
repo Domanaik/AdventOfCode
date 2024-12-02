e1389c58e512ac3dac3ab749e5829003aae2e699
Domanaik <63446985+Domanaik@users.noreply.github.com>
2024-12-02 14:55:18 +0100

    Solution day 2

--- 2024/2/Program.cs
+++ 2024/2/Program.cs
@@ -12,7 +12,7 @@ class Program
         int[][] reports = inputData.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(line => line.Split(' ').Select(int.Parse).ToArray()).ToArray();
 
         int safeReports = reports.Count(report => IsSafe(report) || IsSafeable(report));
-        Thread.Sleep(8000); // Test if Benchmark is working
+
         Console.WriteLine(safeReports);
     }
 
