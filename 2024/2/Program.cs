aa003be4fde1d89a60aaf87567202f1902a27c85
dwantscher_ext <dominique.wantscher@rosenberger.com>
2024-12-02 11:28:28 +0100

    added sleep 10 to test if benchmark is working

--- 2024/2/Program.cs
+++ 2024/2/Program.cs
@@ -12,7 +12,7 @@ class Program
             int[][] reports = lines.Select(line => line.Split(' ').Select(int.Parse).ToArray()).ToArray();
 
             int safeReports = reports.Count(report => IsSafe(report) || IsSafeable(report));
-
+            Thread.Sleep(10000); // Test if Benchmark is working
             Console.WriteLine(safeReports);
         }
         catch (Exception e)
