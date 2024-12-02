451a2f04c1c575b8bea91e9279efc0dffcb6a7ef
dwantscher_ext <dominique.wantscher@rosenberger.com>
2024-12-02 11:24:48 +0100

    Solution day 2

---
+++ 2024/2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            string[] lines = File.ReadAllLines("D:\\GitHub\\AdventOfCode\\2024\\2\\input.txt");
+            int[][] reports = lines.Select(line => line.Split(' ').Select(int.Parse).ToArray()).ToArray();
+
+            int safeReports = reports.Count(report => IsSafe(report) || IsSafeable(report));
+
+            Console.WriteLine(safeReports);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+    }
+
+    static bool IsSafe(int[] report)
+    {
+        bool isIncreasing = true;
+        bool isDecreasing = true;
+
+        for (int i = 0; i < report.Length - 1; i++)
+        {
+            int diff = report[i] - report[i + 1];
+
+            if (Math.Abs(diff) < 1 || Math.Abs(diff) > 3)
+            {
+                return false;
+            }
+
+            if (diff > 0)
+            {
+                isIncreasing = false;
+            }
+
+            if (diff < 0)
+            {
+                isDecreasing = false;
+            }
+        }
+
+        return isIncreasing || isDecreasing;
+    }
+
+    static bool IsSafeable(int[] report)
+    {
+        for (int i = 0; i < report.Length; i++)
+        {
+            int[] newReport = report.Where((_, index) => index != i).ToArray();
+            if (IsSafe(newReport))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
