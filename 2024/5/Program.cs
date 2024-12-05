7dccd2e1a764e655e766ce4d826fb3d0c59e130f
Domanaik <63446985+Domanaik@users.noreply.github.com>
2024-12-05 10:22:32 +0100

    replaced \r\n with Environment.NewLine

---
+++ 2024/5/Program.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode._5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputData = Input.GetSample().Split(Environment.NewLine + Environment.NewLine);
+        string rulesSection = inputData[0];
+        string updatesSection = inputData[1];
+
+        List<(int First, int Second)> rules = [];
+        foreach (string line in rulesSection.Split(Environment.NewLine))
+        {
+            int[] parts = line.Split('|').Select(int.Parse).ToArray();
+            rules.Add((parts[0], parts[1]));
+        }
+
+        List<List<int>> updates = updatesSection.Split(Environment.NewLine).Select(line => line.Split(',').Select(int.Parse).ToList()).ToList();
+
+        Console.WriteLine("Rules:");
+        foreach ((int First, int Second) in rules)
+        {
+            Console.WriteLine($"{First} -> {Second}");
+        }
+
+        Console.WriteLine("\nUpdates:");
+        foreach (List<int> update in updates)
+        {
+            Console.WriteLine(string.Join(", ", update));
+        }
+    }
+}
