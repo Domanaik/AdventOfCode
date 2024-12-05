d421b22d8f2ce694e9cbfc51cdf712bf193298a7
Domanaik <63446985+Domanaik@users.noreply.github.com>
2024-12-05 11:11:40 +0100

    Solution day 5

--- 2024/5/Program.cs
+++ 2024/5/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string[] inputData = Input.GetSample().Split(Environment.NewLine + Environment.NewLine);
+        string[] inputData = Input.GetInput().Split(Environment.NewLine + Environment.NewLine);
         string rulesSection = inputData[0];
         string updatesSection = inputData[1];
 
@@ -17,16 +17,77 @@ class Program
 
         List<List<int>> updates = updatesSection.Split(Environment.NewLine).Select(line => line.Split(',').Select(int.Parse).ToList()).ToList();
 
-        Console.WriteLine("Rules:");
-        foreach ((int First, int Second) in rules)
+        bool IsCorrectOrder(List<int> update)
         {
-            Console.WriteLine($"{First} -> {Second}");
+            Dictionary<int, int> positions = update.Select((page, index) => (Page: page, Index: index)).ToDictionary(x => x.Page, x => x.Index);
+
+            foreach ((int First, int Second) in rules)
+            {
+                if (positions.ContainsKey(First) && positions.ContainsKey(Second))
+                {
+                    if (positions[First] > positions[Second])
+                        return false;
+                }
+            }
+            return true;
         }
 
-        Console.WriteLine("\nUpdates:");
+        static List<int> TopologicalSort(List<int> update, List<(int First, int Second)> rules)
+        {
+            var graph = new Dictionary<int, List<int>>();
+            var inDegree = new Dictionary<int, int>();
+
+            foreach (var page in update)
+            {
+                graph[page] = new List<int>();
+                inDegree[page] = 0;
+            }
+
+            foreach (var (First, Second) in rules)
+            {
+                if (graph.ContainsKey(First) && graph.ContainsKey(Second))
+                {
+                    graph[First].Add(Second);
+                    inDegree[Second]++;
+                }
+            }
+
+            var queue = new Queue<int>(inDegree.Where(kv => kv.Value == 0).Select(kv => kv.Key));
+            var sorted = new List<int>();
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                sorted.Add(current);
+
+                foreach (var neighbor in graph[current])
+                {
+                    inDegree[neighbor]--;
+                    if (inDegree[neighbor] == 0)
+                        queue.Enqueue(neighbor);
+                }
+            }
+
+            return sorted;
+        }
+
+        int partOneSum = 0;
+        int partTwoSum = 0;
+
         foreach (List<int> update in updates)
         {
-            Console.WriteLine(string.Join(", ", update));
+            if (IsCorrectOrder(update))
+            {
+                partOneSum += update[(update.Count - 1) / 2];
+            }
+            else
+            {
+                var correctedUpdate = TopologicalSort(update, rules);
+                partTwoSum += correctedUpdate[(correctedUpdate.Count - 1) / 2];
+            }
         }
+
+        Console.WriteLine(partOneSum);
+        Console.WriteLine(partTwoSum);
     }
 }
