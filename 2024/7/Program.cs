297f16cf7a54a5d946d4479bd835cec59cebab5b
Domanaik <63446985+Domanaik@users.noreply.github.com>
2024-12-07 12:35:52 +0100

    Soluton day 7

---
+++ 2024/7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.IO.Pipelines;
+using System.Threading.Tasks.Dataflow;
+using AdventOfCode._7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputData = Input.GetInput().Split(Environment.NewLine);
+
+        long totalCalibrationResult = 0;
+        const string operators = "+*|";
+
+        foreach (string line in inputData)
+        {
+            string[] parts = line.Split(':');
+            long result = long.Parse(parts[0]);
+            int[] numbers = Array.ConvertAll(parts[1].Trim().Split(' '), int.Parse);
+
+            if (TestIfPossible(result, numbers, 0, numbers[0]))
+            {
+                totalCalibrationResult += result;
+            }
+        }
+
+        static bool TestIfPossible(long result, int[] numbers, int index, long currentResult)
+        {
+            if (index == numbers.Length - 1)
+            {
+                return currentResult == result;
+            }
+
+            foreach (char op in operators)
+            {
+                int nextNumber = numbers[index + 1];
+                long newResult = op switch
+                {
+                    '+' => currentResult + nextNumber,
+                    '*' => currentResult * nextNumber,
+                    _ => long.Parse(currentResult.ToString() + nextNumber.ToString())
+                };
+
+                if (TestIfPossible(result, numbers, index + 1, newResult))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        Console.WriteLine(totalCalibrationResult);
+    }
+}
