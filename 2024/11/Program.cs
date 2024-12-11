a7a4f599273f38fecb74851e2e9b7d3e53e42778
Domanaik <63446985+Domanaik@users.noreply.github.com>
2024-12-11 16:31:35 +0100

    cache version WIP

--- 2024/11/Program.cs
+++ 2024/11/Program.cs
@@ -4,57 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<int> inputData = Input.GetInput().Split(' ').Select(int.Parse).ToList();
+        List<int> inputData = Input.GetSample1().Split(' ').Select(int.Parse).ToList();
 
-        int blinkTimes = 75;
+        int blinkTimes = 6;
+        Dictionary<(long, int), List<long>> cache = [];
         List<long> finalStones = [];
 
         foreach (int stone in inputData)
         {
-            List<long> stonesAfterAllBlinks = [stone];
-
-            for (int i = 0; i < blinkTimes; i++)
-            {
-                Console.WriteLine($@"({i + 1}/{blinkTimes})");
-                List<long> transformedStones = [];
-
-                foreach (long currentStone in stonesAfterAllBlinks)
-                {
-                    transformedStones.AddRange(ApplyBlink(currentStone));
-                }
-
-                stonesAfterAllBlinks = transformedStones;
-            }
-
-            finalStones.AddRange(stonesAfterAllBlinks);
+            finalStones.AddRange(ApplyBlink(stone, blinkTimes, cache));
         }
 
         Console.WriteLine($"Total stones after {blinkTimes} blinks: {finalStones.Count}");
     }
 
-    static List<long> ApplyBlink(long stone)
+    static List<long> ApplyBlink(long stone, int blinksLeft, Dictionary<(long, int), List<long>> cache)
     {
-        //Console.Write($@" {stone}");
+        if (cache.TryGetValue((stone, blinksLeft), out var cached))
+        {
+            return cached;
+        }
+
+        if (blinksLeft == 0)
+        {
+            return [stone];
+        }
+
         List<long> newStones = [];
         int numDigits = (int)Math.Floor(Math.Log10(stone) + 1);
 
         if (stone == 0)
         {
-            newStones.Add(1);
-            //Console.WriteLine(" => 1 (Rule 1)");
+            newStones.AddRange(ApplyBlink(1, blinksLeft - 1, cache));
         }
         else if (numDigits % 2 == 0)
         {
             long divisor = (long)Math.Pow(10, numDigits / 2);
-            newStones.Add(stone / divisor);
-            newStones.Add(stone % divisor);
-            //Console.WriteLine($@" => {stone / divisor} & {stone % divisor} (Rule 2)");
+            newStones.AddRange(ApplyBlink(stone / divisor, blinksLeft - 1, cache));
+            newStones.AddRange(ApplyBlink(stone % divisor, blinksLeft - 1, cache));
         }
         else
         {
-            newStones.Add(stone * 2024);
-            //Console.WriteLine($@" => {stone * 2024} (Rule 3)");
+            newStones.AddRange(ApplyBlink(stone * 2024, blinksLeft - 1, cache));
         }
+
+        cache[(stone, blinksLeft)] = newStones;
 
         return newStones;
     }
