38a5241093dc6241b0db5569f5826ea192899202
Domanaik <63446985+Domanaik@users.noreply.github.com>
2024-12-11 11:36:44 +0100

    still to slow :(

--- 2024/11/Program.cs
+++ 2024/11/Program.cs
@@ -4,53 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<long> inputData = Input.GetSample2().Split(' ').Select(long.Parse).ToList();
+        List<int> inputData = Input.GetInput().Split(' ').Select(int.Parse).ToList();
 
-        List<long> newStoneList = [];
+        int blinkTimes = 75;
+        List<long> finalStones = [];
 
-        int blinkTimes = 25;
-
-        for (int i = 0; i < blinkTimes; i++)
+        foreach (int stone in inputData)
         {
-            Console.WriteLine($@"{i} of {blinkTimes}");
-            inputData = ChangeStoneAtBlink(inputData);
+            List<long> stonesAfterAllBlinks = [stone];
+
+            for (int i = 0; i < blinkTimes; i++)
+            {
+                Console.WriteLine($@"({i + 1}/{blinkTimes})");
+                List<long> transformedStones = [];
+
+                foreach (long currentStone in stonesAfterAllBlinks)
+                {
+                    //Console.Write($@" {currentStone}");
+                    transformedStones.AddRange(ApplyBlink(currentStone));
+                }
+
+                stonesAfterAllBlinks = transformedStones;
+            }
+
+            finalStones.AddRange(stonesAfterAllBlinks);
         }
 
-        Console.WriteLine(inputData.Count);
+        Console.WriteLine($"Total stones after {blinkTimes} blinks: {finalStones.Count}");
     }
 
-    static List<long> ChangeStoneAtBlink(List<long> stoneList)
+    static List<long> ApplyBlink(long stone)
     {
-        List<long> newStoneList = [];
+        List<long> newStones = [];
+        int numDigits = (int)Math.Floor(Math.Log10(stone) + 1);
 
-        foreach (long stone in stoneList)
+        if (stone == 0)
         {
-            string stoneNumber = stone.ToString();
-            Console.Write(stone);
-
-            if (stone == 0)
-            {
-                newStoneList.Add(1);
-                Console.WriteLine(" => 1 (Rule 1)");
-            }
-            else
-            {
-                int numDigits = (int)Math.Floor(Math.Log10(stone) + 1);
-                if (numDigits % 2 == 0)
-                {
-                    long divisor = (int)Math.Pow(10, numDigits / 2);
-                    newStoneList.Add(stone / divisor);
-                    newStoneList.Add(stone % divisor);
-                    Console.WriteLine($@" => {stone / divisor} & {stone % divisor} (Rule 2)");
-                }
-                else
-                {
-                    newStoneList.Add(stone * 2024);
-                    Console.WriteLine($@" => {stone * 2024} (Rule 3)");
-                }
-            }
+            newStones.Add(1);
+            //Console.WriteLine(" => 1 (Rule 1)");
+        }
+        else if (numDigits % 2 == 0)
+        {
+            long divisor = (long)Math.Pow(10, numDigits / 2);
+            newStones.Add(stone / divisor);
+            newStones.Add(stone % divisor);
+            //Console.WriteLine($@" => {stone / divisor} & {stone % divisor} (Rule 2)");
+        }
+        else
+        {
+            newStones.Add(stone * 2024);
+            //Console.WriteLine($@" => {stone * 2024} (Rule 3)");
         }
 
-        return newStoneList;
+        return newStones;
     }
 }
