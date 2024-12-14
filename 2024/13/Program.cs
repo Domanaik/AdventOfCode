206ce6ddfa5d43b651a2d8432901c6563a5887f0
Domanaik <63446985+Domanaik@users.noreply.github.com>
2024-12-14 11:01:36 +0100

    Solution day 13 part 1

--- 2024/13/Program.cs
+++ 2024/13/Program.cs
@@ -1,12 +1,14 @@
-﻿using AdventOfCode._13;
+﻿using System.Net.Http.Headers;
+using AdventOfCode._13;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string[] inputData = Input.GetSample().Split(Environment.NewLine + Environment.NewLine);
+        string[] inputData = Input.GetInput().Split(Environment.NewLine + Environment.NewLine);
 
-        List<(int, (int x, int y))> clawMachines = [];
+        int totalTokens = 0;
 
         foreach (var block in inputData)
         {
@@ -16,10 +18,15 @@ class Program
             var buttonB = ParseCoordinates(lines[1]);
             var prize = ParseCoordinates(lines[2]);
 
-            Console.WriteLine($"Button A: X+{buttonA.X}, Y+{buttonA.Y}");
-            Console.WriteLine($"Button B: X+{buttonB.X}, Y+{buttonB.Y}");
-            Console.WriteLine($"Prize: X={prize.X}, Y={prize.Y}\n");
+            (int? PushesA, int? PushesB, int Cost) = GetBestCombination(buttonA, buttonB, prize);
+
+            if (PushesA.HasValue && PushesB.HasValue)
+            {
+                totalTokens += Cost;
+            }
         }
+
+        Console.WriteLine(totalTokens);
     }
 
     static (int X, int Y) ParseCoordinates(string line)
@@ -28,5 +35,43 @@ class Program
         int x = int.Parse(parts[0].Split(['+', '='], StringSplitOptions.RemoveEmptyEntries)[1]);
         int y = int.Parse(parts[1].Split(['+', '='], StringSplitOptions.RemoveEmptyEntries)[1]);
         return (x, y);
+    }
+
+    static (int? PushesA, int? PushesB, int Cost) GetBestCombination((int X, int Y) buttonA, (int X, int Y) buttonB, (int X, int Y) prize)
+    {
+        const int maxPresses = 100;
+        const int costA = 3;
+        const int costB = 1;
+        int? bestA = null;
+        int? bestB = null;
+        int minCost = int.MaxValue;
+
+        for (int a = 0; a <= maxPresses; a++)
+        {
+            for (int b = 0; b <= maxPresses; b++)
+            {
+                int totalX = a * buttonA.X + b * buttonB.X;
+                int totalY = a * buttonA.Y + b * buttonB.Y;
+
+                if (totalX > prize.X || totalY > prize.Y)
+                {
+                    break;
+                }
+
+                if (totalX == prize.X && totalY == prize.Y)
+                {
+                    int cost = a * costA + b * costB;
+
+                    if (cost < minCost)
+                    {
+                        bestA = a;
+                        bestB = b;
+                        minCost = cost;
+                    }
+                }
+            }
+        }
+
+        return (bestA, bestB, minCost);
     }
 }
