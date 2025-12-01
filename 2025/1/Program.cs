3e7209f31560dc98c46784c445ed7bc2395694a9
Domanaik <63446985+Domanaik@users.noreply.github.com>
2025-12-02 00:26:29 +0100

    Initial commit

---
+++ 2025/1/Program.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCode._1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputData = Input.GetInput().Split(Environment.NewLine);
+        int start = 50;
+        int password1 = 0;
+        int password2 = 0;
+
+        foreach (string line in inputData)
+        {
+            int value = int.Parse(line[1..]);
+
+            if (line[0] == 'R')
+            {
+                start += value;
+                while (start >= 100)
+                {
+                    start -= 100;
+                    if (start != 0)
+                        password2++;
+                }
+            }
+            else
+            {
+                if (start == 0)
+                    password2--;
+
+                start -= value;
+
+                while (start < 0)
+                {
+                    start += 100;
+                    password2++;
+                }
+            }
+
+            if (start == 0)
+                password1++;
+        }
+        password2 += password1;
+
+        Console.WriteLine("Part 1: " + password1);
+        Console.WriteLine("Part 2: " + password2);
+    }
+}
