18aee5d3635baba3239da7d20c443f9e62d0f91f
Dominique <63446985+Domanaik@users.noreply.github.com>
2025-12-02 15:35:56 +0100

    Refactor input handling and logic adjustments

--- 2025/1/Program.cs
+++ 2025/1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string[] inputData = Input.GetInput().Split(Environment.NewLine);
+        string[] inputData = GetInput().Split(Environment.NewLine);
         int start = 50;
         int password1 = 0;
         int password2 = 0;
@@ -12,11 +12,10 @@ class Program
         foreach (string line in inputData)
         {
             int value = int.Parse(line[1..]);
-
             if (line[0] == 'R')
             {
                 start += value;
-                while (start >= 100)
+                while (start > 99)
                 {
                     start -= 100;
                     if (start > 0)
@@ -25,19 +24,20 @@ class Program
             }
             else
             {
+                if (start == 0)
+                    password2--;
                 start -= value;
                 while (start < 0)
                 {
                     start += 100;
-                    if (start >= 0)
-                        password2++;
+                    password2++;
                 }
             }
             if (start == 0)
                 password1++;
         }
         password2 += password1;
-
+            
         Console.WriteLine("Part 1: " + password1);
         Console.WriteLine("Part 2: " + password2);
     }
