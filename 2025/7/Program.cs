7de5765e7dae86609d35d54830652217a675d5d3
Domanaik <63446985+Domanaik@users.noreply.github.com>
2025-12-08 17:18:03 +0100

    Day 7, part 1

---
+++ 2025/7/Program.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCode._7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputData = Input.GetInput().Split(Environment.NewLine);
+
+        int beamSplit = 0;
+
+        int rows = inputData.Length;
+        int cols = inputData[0].Length;
+
+        char[,] map = new char[rows, cols];
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < cols; x++)
+            {
+                map[y, x] = inputData[y][x];
+            }
+        }
+
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < cols; x++)
+            {
+                if (map[y, x] == 'S' || map[y, x] == '|')
+                {
+                    if (y + 1 >= rows)
+                        break;
+
+                    if (map[y + 1, x] == '^')
+                    {
+                        beamSplit++;
+                        if (map[y + 1, x - 1] != '|')
+                            map[y + 1, x - 1] = '|';
+                        if (map[y + 1, x + 1] != '|')
+                            map[y + 1, x + 1] = '|';
+                    }
+                    else
+                    {
+                        map[y + 1, x] = '|';
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine("Part 1: " + beamSplit);
+    }
+}
