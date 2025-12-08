2bd1afdb3e173e78e15c2df68f94e8f002bd3595
Domanaik <63446985+Domanaik@users.noreply.github.com>
2025-12-08 16:21:06 +0100

    Day 5 part 1

---
+++ 2025/5/Program.cs
@@ -0,0 +1,42 @@
+﻿using AdventOfCode._5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputData = Input.GetInput().Split(Environment.NewLine + Environment.NewLine);
+
+        string[] freshIngredientIDRanges = inputData[0].Split(Environment.NewLine);
+        string[] availableIngredientIDs = inputData[1].Split(Environment.NewLine);
+
+        int fresh1 = 0;
+
+        foreach (string ingredient in availableIngredientIDs)
+        {
+            long id = long.Parse(ingredient);
+            foreach (string range in freshIngredientIDRanges)
+            {
+                if (id >= long.Parse(range.Split('-')[0]) && id <= long.Parse(range.Split('-')[1]))
+                {
+                    fresh1++;
+                    break;
+                }
+            }
+        }
+
+        List<long> freshIngredientIDs = [];
+
+        foreach (string range in freshIngredientIDRanges)
+        {
+            for (long i = long.Parse(range.Split('-')[0]); i <= long.Parse(range.Split('-')[1]); i++)
+            {
+                if (freshIngredientIDs.Contains(i))
+                    continue;
+
+                freshIngredientIDs.Add(i);
+            }
+        }
+        Console.WriteLine("Part 1: " + fresh1);
+        Console.WriteLine("Part 2: " + freshIngredientIDs.Count);
+    }
+}
